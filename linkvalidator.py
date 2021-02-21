b1634295a24f7b201adb0793db8a547d22cf773d
Price-Tracker-In <68950357+Price-Tracker-In@users.noreply.github.com>
2021-02-21 20:40:35 +0530

    Class Id updated

--- linkvalidator.py
+++ linkvalidator.py
@@ -4,11 +4,10 @@ from bs4 import BeautifulSoup as bs
 
 # In[2]:
 
-
 def check(url):
     page = r.get(url)
     soup = bs(page.content,"html.parser")
-    if soup.find_all("div",{"class":"_1vC4OE _3qQ9m1"}):
+    if soup.find_all("div",{"class":"_30jeq3 _16Jk6d"}):
         return True
     else:
         return False
